4a10b159b4a1a0a69bdeed77e3d02f043394d214
anilkumarsarvana <160317263+anilkumarsarvana@users.noreply.github.com>
2026-02-28 14:12:23 +0530

    Remove quotes from variable references in resource.tf

--- day-2/resource.tf
+++ day-2/resource.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "myinstance" {
 
   tags = {
-    Name = "var.iname"
+    Name = var.iname
   }
 
-  ami               = "var.iamiid"
-  instance_type     = "var.itype"
-  key_name          = "var.ikey"
-  availability_zone = "var.izone"
+  ami               = var.iamiid
+  instance_type     = var.itype
+  key_name          = var.ikey
+  availability_zone = var.izone
   count             = var.icount
 
   root_block_device {
