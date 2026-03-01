f729b3832d47000b863f4f10e003497115c861fd
anilkumarsarvana <160317263+anilkumarsarvana@users.noreply.github.com>
2026-03-01 19:01:24 +0530

    Taint and lifecycle

--- day-4/Taint and Lifecycle/main.tf
+++ day-4/Taint and Lifecycle/main.tf
@@ -1,1 +1,33 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+locals {
+  itype = {
+    dev  = "t3.micro"
+    test = "t3.small"
+  }
+}
+
+
+resource "aws_instance" "myinstance" {
+
+  tags = {
+    Name = "${terraform.workspace}-server"
+  }
+
+  ami               = "ami-056335ec4a8783947"
+  instance_type     = local.itype[terraform.workspace]
+  key_name          = "DEVOPSWITHMULTICLOUD"
+  availability_zone = "eu-north-1a"
+  count             = "2"
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  root_block_device {
+
+    volume_size = "10"
+  }
+}
 
